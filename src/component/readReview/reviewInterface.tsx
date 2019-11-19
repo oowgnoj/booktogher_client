f54b3baa621c67e0548620c494774656ed4924cd
immime12697@gmail.com <ginsum>
2019-11-19 16:27:19 +0900

    Add readReview Componet

---
+++ src/component/readReview/reviewInterface.tsx
@@ -0,0 +1,41 @@
+export interface IAuthor {
+  id: string;
+  image: string;
+  name: string;
+  profile: string;
+}
+
+export interface IReview {
+  id: string;
+  author: IAuthor;
+  contents: string;
+  likes: string[];
+  published: boolean;
+  thumbnail: string;
+  title: string;
+}
+
+export interface IProps {
+  review: IReview;
+  bookList : IBook[]; 
+}
+
+export interface IReviewProps {
+  review: IReview; 
+}
+export interface IBookState {
+  bookList : IBook[];
+}
+export interface IReviewState {
+  reviewList: IReview[];
+}
+
+export interface IBook {
+  id: string;
+  authors: [ string ];
+  contents: string;
+  rating: number;
+  thumbnail: string;
+  title: string;
+}
+
