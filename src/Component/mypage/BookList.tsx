0808d7c63d037563c694603116c758a05500a499
jpark <mangtaob@gmail.com>
2019-11-19 23:55:08 +0900

    PAUSE beautiful dnd

--- src/Component/mypage/BookList.tsx
+++ src/Component/mypage/BookList.tsx
@@ -58,3 +58,56 @@ const App: React.FC<IProps> = ({ User }) => {
 };
 
 export default App;
+
+const App = () => {
+  const [rows, setRows] = React.useState([
+    { id: aId, label: "a", urls: [] },
+    {
+      id: unrankedId,
+      label: "unranked",
+      urls: [
+        "https://www.ssbwiki.com/images/thumb/b/b3/Olimar_SSBU.png/500px-Olimar_SSBU.png",
+        "https://www.ssbwiki.com/images/thumb/b/b0/Olimar-Alt4_SSBU.png/500px-Olimar-Alt4_SSBU.png"
+      ]
+    }
+  ]);
+
+  return (
+    <DragDropContext
+      onDragEnd={({ destination, source }) => {
+        // // dropped outside the list
+        if (!destination) {
+          return;
+        }
+
+        setRows(reorderRows(rows, source, destination));
+      }}
+    >
+      <div>
+        <button
+          onClick={() => {
+            setRows([
+              {
+                id: generate(),
+                label: "",
+                urls: []
+              },
+              ...rows
+            ]);
+          }}
+        >
+          add row
+        </button>
+        {rows.map(row => (
+          <AuthorList
+            internalScroll
+            key={row.id}
+            listId={row.id}
+            listType="CARD"
+            row={row}
+          />
+        ))}
+      </div>
+    </DragDropContext>
+  );
+};
