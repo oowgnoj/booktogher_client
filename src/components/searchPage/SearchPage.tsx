ecfc9d0ef929db313ea2dd13644f3ee0b82087dc
immime12697@gmail.com <ginsum>
2019-11-24 16:32:10 +0900

    Add search select Box

--- src/components/searchPage/SearchPage.tsx
+++ src/components/searchPage/SearchPage.tsx
@@ -2,11 +2,40 @@ import React, { ReactElement } from "react";
 import SearchBooks from "./SearchBooks"
 import SearchReviews from "./SearchReviews"
 
+interface IState {
+  selectBook: boolean;
+}
 const SearchPage = (): ReactElement => {
+  const state : IState={
+    selectBook : true
+  }
+  const handleChangeSelect = (e: any) => {
+    console.log(state)
+    if(e.value === "book"){
+      state.selectBook = true
+    } else {
+      state.selectBook = false
+    } 
+  }
   return (
     <div>
-      <SearchBooks />
-      <SearchReviews />     
+      <div className = "search-area">
+        <div className = "search-select">
+          <div className="uk-margin">
+              <div className="uk-form-controls">              
+                  <select 
+                    className="uk-select" 
+                    id="form-stacked-select"
+                    onChange ={(e) => handleChangeSelect(e)}>
+                      <option value="book">책</option>
+                      <option value="review">서평</option>
+                  </select>  
+              </div>
+          </div>
+        </div>
+      </div>
+
+      {state.selectBook ? <SearchBooks /> :  <SearchReviews /> } 
     </div>
   );
 };
