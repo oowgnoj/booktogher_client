de5a331fe6c5ab799fa5b4cfe0cb6993d960b4be
jpark <mangtaob@gmail.com>
2019-11-27 16:56:59 +0900

    EDIT book index photo

---
+++ src/components/myBook/cards.tsx
@@ -0,0 +1,67 @@
+import React, { ReactElement } from "react";
+
+import "./cards.css";
+
+const Cards: React.FC = (): ReactElement => {
+  return (
+    <div>
+      <div
+        className="uk-container"
+        style={{
+          height: "250px",
+          backgroundRepeat: "no-repeat",
+          WebkitFilter: "grayscale(100%)",
+          opacity: "70%",
+          backgroundImage:
+            "url('https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&auto=format&fit=crop&w=2053&q=80')"
+        }}
+      >
+        <div
+          style={{
+            height: "100px",
+            justifyContent: "space-evenly",
+            bottom: "0",
+            position: "absolute"
+          }}
+        >
+          <div
+            className="uk-card uk-card-primary uk-card-body jongwoo"
+            style={{
+              display: "inline-block",
+              textAlign: "center",
+              width: "80px",
+              height: "100px"
+            }}
+          >
+            <p>Finished Book</p>
+          </div>
+          <div
+            className="uk-card uk-card-primary uk-card-body"
+            style={{
+              display: "inline-block",
+              textAlign: "center",
+              width: "80px",
+              height: "100px"
+            }}
+          >
+            <p>Reading Book</p>
+          </div>
+          <div
+            className="uk-card uk-card-primary uk-card-body"
+            style={{
+              display: "inline-block",
+              textAlign: "center",
+              width: "80px",
+              height: "100px"
+            }}
+          >
+            {" "}
+            <p>TO read</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
