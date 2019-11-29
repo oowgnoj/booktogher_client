a6c273dfcafc095b0c303144bf2f43e8f1082423
immime12697@gmail.com <ginsum>
2019-11-29 11:26:26 +0900

    Fix ReviewList

--- src/components/bookDetail/BookReview.tsx
+++ src/components/bookDetail/BookReview.tsx
@@ -37,22 +37,68 @@ class BookReview extends React.Component<IProps, IReviews> {
   }
 
   public render(): ReactElement {
-    const reviewList: any = this.state.reviews.map((info: IReview) => {
-      if(info.published !== false){
+    const reviewList: any = this.state.reviews.map((review: IReview) => {
+      if(review.published !== false){
         return(
-          <div key ={info._id} className="review-detail">
-            <Link to={`/review/${info._id}`}><h4>{info.title}</h4></Link>
-            <div>{info.author.name}</div>
-            <div>{info.contents.replace(/<[^>]*>?/gm, '')}</div>        
-          </div>
+          <a href={`/review/${review._id}`} style={{ textDecoration: "none" }}>
+            <div className="book_review_list_review">
+                <div>
+                  <div 
+                    className="uk-card  uk-card-body" 
+                    style={{ height: "200px", margin:"20px" }}>
+                    <h3
+                      className="uk-card-title"
+                      style={{ fontSize: "1.1em", textAlign: "left" }}
+                    >
+                      <span
+                        style={{
+                          color: "#5b88b2",
+                          fontSize: "60%",
+                          marginRight: "5px"
+                        }}
+                      >
+                        서평
+                      </span>{" "}
+                      {review.title}
+                    </h3>
+
+                    <p
+                      style={{
+                        color: "gray",
+                        fontSize: "0.9em",
+                        textAlign: "justify"
+                      }}
+                    >
+                      {review.contents.length > 80
+                        ? review.contents.replace(/<[^>]*>?/gm, "").slice(0, 80) +
+                          "..."
+                        : review.contents.replace(/<[^>]*>?/gm, "")}
+                    </p>
+                    <p
+                      style={{
+                        color: "gray",
+                        textAlign: "right",
+                        marginTop: "-10px",
+                        marginBottom: "-10px",
+                        fontSize: "0.7em",
+                        fontStyle: "italic"
+                      }}
+                    >
+                      by {review.author.name}
+                    </p>
+                  </div>
+                </div>
+            </div>
+          </a>
           ) 
         }   
       })
     return (
       <div className ="book-reveiw">
         <h4>이 책의 서평들</h4>
-        <div>{this.state.reviews.length === 0 ?
-        "이 책의 서평이 없습니다." : reviewList}</div>
+        <div className ="uk-flex uk-flex-left@s uk-flex-wrap">
+          {this.state.reviews.length === 0 ?
+          "이 책의 서평이 없습니다." : reviewList}</div>
       </div>
     );
   }
