dc68497ca725db11d5648f4ca276e56ee3bf4622
immime12697@gmail.com <ginsum>
2019-11-30 10:05:58 +0900

    Fix bug

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,15 +1,15 @@
 import React, { ReactElement } from "react";
 import logo from "../../Asset/images/logo.png";
 
-const Footer = (): ReactElement => {  
-  const style: any ={
+const Footer = (): ReactElement => {
+  const style: any = {
     width: "100%",
     height: "150px",
     backgroundColor: "#eee",
     marginTop: "160px"
-  }
+  };
   return (
-    <div style = {style}>
+    <div style={style}>
       <img
         src={logo}
         alt="로고"
@@ -19,9 +19,11 @@ const Footer = (): ReactElement => {
           marginLeft: "20%"
         }}
       />
-      <span style ={{marginLeft: "40%", paddingTop:"80px"}}>© 2019, BookTogether</span>
+      <span style={{ marginLeft: "40%", paddingTop: "80px" }}>
+        © 2019, BookTogether
+      </span>
     </div>
-  ) 
+  );
 };
 
 export default Footer;
